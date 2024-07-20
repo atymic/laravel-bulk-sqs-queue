54c10de8b06a0fc30071fd3361b0fa3422c4b8d1
atymic <atymicq@gmail.com>
2024-07-20 11:08:04 +1000

    fix: correct typing for error

--- src/Exception/BulkSqsDispatchFailed.php
+++ src/Exception/BulkSqsDispatchFailed.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Atymic\AsyncSqsQueue\Exception;
 
+use Aws\Result;
 use Exception;
 use Illuminate\Support\Collection;
 
@@ -11,7 +12,7 @@ class BulkSqsDispatchFailed extends Exception
     public static function withFailureResponses(Collection $failures): self
     {
         $message = $failures
-            ->map(fn (array $fail) => sprintf('%s failed with %s, message `%s`', $fail['Id'], $fail['Code'], $fail['Message']))
+            ->map(fn (Result $fail) => sprintf('%s failed with %s, message `%s`', $fail['Id'], $fail['Code'], $fail['Message']))
             ->prepend('Bulk dispatch failed, errors:')
             ->join("\n");
 
